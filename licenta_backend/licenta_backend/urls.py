eedec2214a5a240d29f2e9efb39e0b31af071465
gk_rogstrix <krisztian_gombos@outlook.hu>
2024-02-11 21:54:46 +0200

    edit urls

--- licenta_backend/licenta_backend/urls.py
+++ licenta_backend/licenta_backend/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('authentication/', include('authentication.urls')),
+    path('furniture/', include('furniture.urls')),
     path('api-auth/', include('rest_framework.urls'))
 ]
